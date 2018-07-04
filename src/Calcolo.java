bc092d5e3691b09c83297ba4294b2450a7fbd031
Giovanni <Xivolg98@gmail.com>
2018-07-04 18:08:46 +0200

    Calcola la soluzione ma non stampa xml

--- src/Calcolo.java
+++ src/Calcolo.java
@@ -119,55 +119,52 @@ public class Calcolo {
 	public static void Flooding(ArrayList<Integer> nodi,ArrayList<Integer> quantitaLink,ArrayList<Integer> link) {
 		int contaDa=0;
 		int contaA=0;
+		int contatore=1;
+		int contatoreSecondario=1;
 		ArrayList<ArrayList<Integer>> indirizzi = new ArrayList<ArrayList<Integer>>();
 		for(int i=0;i<nodi.size();i++) {
 			ArrayList<Integer> linkDelNodo = new ArrayList<Integer>();
 			contaA=contaDa + quantitaLink.get(i);
-			System.out.println("contaDa "+contaDa+" contaA "+contaA);
-			System.out.println("link ");
+			//			System.out.println("contaDa "+contaDa+" contaA "+contaA);
+			//			System.out.println("link ");
 			for(int j=contaDa;j<contaA;j++) {
 				linkDelNodo.add(link.get(j));
 			}
 			indirizzi.add(linkDelNodo);
-			for(int puntatore: linkDelNodo) {
-				System.out.println(puntatore);
-			}
 			contaDa = contaDa +quantitaLink.get(i);
 		}
 		for(int i=0;i<nodi.size();i++) {
 			ArrayList<Integer> copia = new ArrayList<Integer>(nodi);
 			copia.remove(i);
-			//			System.out.println("VVV"+copia + nodi);
 			ArrayList<Integer> linkDaPassare = new ArrayList<Integer>(indirizzi.get(i));
-			//			System.out.println("FFF" + linkDaPassare);
 			ArrayList<Integer> linkAggiornati = new ArrayList<Integer>();
+//			System.out.println("Nuovo problema "+copia);
 			do {
 				linkAggiornati.clear();
 				for(int puntatore1 : linkDaPassare) {
 					for(int k=0;k<copia.size();k++) {
 						if(puntatore1==copia.get(k)) {
 							copia.remove(k);
-							System.out.println("GG"+copia);
+//							System.out.println("GG"+copia);
 						}
 					}
 				}
+				contatore++;
+//				System.out.println("GG"+copia);
+//				System.out.println("finito il primo for" + contatore);
 				for(int puntatore1 : linkDaPassare) {
-					//					for(int k=0;k<copia.size();k++) {
-					//						if(puntatore1==copia.get(k)) {
-					//							copia.remove(k);
-					//							System.out.println("GG"+copia + nodi);
-					//						}
-					//					}
 					for(int puntatore2:indirizzi.get(puntatore1)) {
 						for(int j=0;j<copia.size();j++) {
 							if(puntatore2==copia.get(j)) {
 								linkAggiornati.add(puntatore2);
-								copia.remove(j);
-								System.out.println("GG"+copia + copia.isEmpty());
+//								copia.remove(j);
+//								System.out.println("GG"+copia + copia.isEmpty());
 							}
 						}
 					}
 				}
+//				System.out.println("GG"+copia + copia.isEmpty());
+//				System.out.println("finito il secondo for");
 				linkDaPassare.clear();
 				for(int puntatore3 : linkAggiornati) 
 				{
@@ -175,11 +172,15 @@ public class Calcolo {
 				}
 			}
 			while(!copia.isEmpty());
+			if(i==0) {
+				contatoreSecondario=contatore;
+			}
+			if(contatoreSecondario>contatore) {
+				contatoreSecondario=contatore;
+			}
+			contatore=1;
 		}
-
-
-		System.out.println("Tadah "+indirizzi);
-
+		System.out.println("Soluzione " + contatoreSecondario);
 	}
 
 
